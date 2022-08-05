6c5d8dac2f709d2e0402d19636d8fa4ff4fc275e
David Ljunggren <dljungg.kth.se>
2022-08-05 18:18:25 +0200

    part0 and part1 tut OK

---
+++ part1/part1/src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p> Hello {props.name}, you are {props.age} years of age, and your favorite animal are {props.favoriteAnimal}</p>
+
+    </div>
+  )
+}
+const App = () => {
+const name ='David'
+const age = 28
+const favoriteAnimal = "cats"
+  return (
+  <div>
+  <h1> Greetings</h1>
+  
+  <Hello name = "Louise" age={15+5+3} favoriteAnimal="cats"/>
+  <Hello name = {name} age={age} favoriteAnimal={favoriteAnimal}/>
+  </div>
+  )
+  }
+export default App;
